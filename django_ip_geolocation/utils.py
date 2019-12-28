7d7d99bf2cfb9223971dc13837245cb6bced89e1
SkanderBM <skander.bmahmoud@gmail.com>
2019-12-28 13:34:18 +0100

    default backend

--- django_ip_geolocation/utils.py
+++ django_ip_geolocation/utils.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.utils.module_loading import import_string
+import logging
 
 def get_remote_ip_from_request(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
@@ -11,9 +12,12 @@ def get_remote_ip_from_request(request):
 
 
 def get_geolocation_backend_cls():
-    geolocation_backend = settings.GEOLOCATION_BACKEND
-    if not geolocation_backend:
-        geolocation_backend = 'django_ip_geolocation.backends.IPGeolocationAPI'
+    geolocation_backend = 'django_ip_geolocation.backends.IPGeolocationAPI'
+    try:
+        geolocation_backend = settings.GEOLOCATION_BACKEND
+    except Exception:
+        logging.warning('Using default logger %s', geolocation_backend, exc_info=True)
+
     
     geolocation_backend_cls = import_string(geolocation_backend)
 
