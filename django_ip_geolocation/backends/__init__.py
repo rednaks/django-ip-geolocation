0a7f169849b35fc59e1c3ec35b368ba226f27500
SkanderBM <skander.bmahmoud@gmail.com>
2019-12-28 15:19:49 +0100

    reorder

--- django_ip_geolocation/backends/__init__.py
+++ django_ip_geolocation/backends/__init__.py
@@ -1,4 +1,4 @@
-from django_ip_geolocation.backends.ipgeolocationapi import IPGeolocationAPI
 from django_ip_geolocation.backends.base import GeolocationBackend
+from django_ip_geolocation.backends.ipgeolocationapi import IPGeolocationAPI
 
 __all__ = ['GeolocationBackend', 'IPGeolocationAPI']
