47765e6a245cc2382ba3659a96ce32c52cd61898
SkanderBM <skander.bmahmoud@gmail.com>
2020-01-25 16:07:23 +0100

    integrating ip2location.com ws #7

---
+++ django_ip_geolocation/backends/ip2locationcom.py
@@ -0,0 +1,37 @@
+import requests
+from django_ip_geolocation.backends import GeolocationBackend
+from django_ip_geolocation import settings
+
+
+class IP2LocationCom(GeolocationBackend):
+
+    def geolocate(self):
+        api_key = settings.IP_GEOLOCATION_SETTINGS.get('BACKEND_API_KEY')
+        extra_params = settings.IP_GEOLOCATION_SETTINGS.get('BACKEND_EXTRA_PARAMS', {})
+
+        if not api_key:
+            msg = "BACKEND_API_KEY is required. Please provide an API_KEY in IP_GEOLOCATION_SETTINGS"
+            raise Exception(msg)
+
+        payload = {
+            'key': api_key,
+            'ip': self._ip,
+            'package': extra_params.get('package', 'WS24')
+        }
+
+        url = 'https://api.ip2location.com/v2/'
+        res = requests.get(url, data=payload)
+        if res.ok:
+            self._raw_data = res.json()
+
+    def _parse(self):
+        self._continent = self._raw_data.get('continent_name')
+        self._country = {
+            'code': self._raw_data.get('country_code'),
+            'name': self._raw_data.get('country_name'),
+        }
+
+        self._geo_data = {
+            'latitude': self._raw_data.get('latitude'),
+            'longitude': self._raw_data.get('longitude'),
+        }
