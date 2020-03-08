9fdec78aef2be50656f22949f17795ce8d5f6503
SkanderBM <skander.bmahmoud@gmail.com>
2020-03-08 12:54:56 +0100

    bump version 1.5.2 -> 1.5.3 and update supported python versions

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 setup(
     name='django-ip-geolocation',
-    version='1.5.2',
+    version='1.5.3',
     author='Skander Ben Mahmoud',
     author_email='skander.bmahmoud@gmail.com',
     packages=find_packages(exclude=("tests", "docs")),
@@ -32,7 +32,9 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
