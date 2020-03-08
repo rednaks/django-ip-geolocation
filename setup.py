34562d0f1495b50f685b9bff85e39846bc93ffe3
SkanderBM <skander.bmahmoud@gmail.com>
2020-03-08 12:28:12 +0100

    bump version 1.5.1 -> 1.5.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 setup(
     name='django-ip-geolocation',
-    version='1.5.1',
+    version='1.5.2',
     author='Skander Ben Mahmoud',
     author_email='skander.bmahmoud@gmail.com',
     packages=find_packages(exclude=("tests", "docs")),
